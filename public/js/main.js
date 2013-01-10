dc1eaffdba0d9428b3fd7d82330a52ec3ab930cd
James Kozianski <koz@chromium.org>
2013-01-11 00:41:37 +1100

    Merge server and client code.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,32 @@
 // main.js
 
 function MessageListCtrl($scope) {
+  var socket = io.connect();
+  socket.on('send-chat', function(data) {
+    $scope.$apply(function() {
+      $scope.receiveMessage(data);
+    });
+  });
+
+  $scope.receiveMessage = function(data) {
+    $scope.messages.push(
+        {content: data.login + ": " + data.text});
+  };
+
+  $scope.message = '';
   $scope.messages = [
+    /*
     {"content": "Some Lorem Ipsum goes here"},
     {"content": "And some more Ipsum Wipsum can go here"},
     {"content": "And some more stuff can go here"}
+    */
   ];
+
+  $scope.submitForm = function() {
+    if ($scope.message == '')
+      return;
+    socket.emit('send-chat', {text: $scope.message});
+    $scope.message = '';
+    console.log("HI");
+  };
 };
