6d5db0e86f73c69f0feb7aa50d166baaf7c1c1f3
KamaKAzii <spskamakazii@gmail.com>
2013-01-11 00:10:57 +1100

    Cleaned up some styles for chat room and fiddled with some angularjs

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+// main.js
+
+function MessageListCtrl($scope) {
+  $scope.messages = [
+    {"content": "Some Lorem Ipsum goes here"},
+    {"content": "And some more Ipsum Wipsum can go here"},
+    {"content": "And some more stuff can go here"}
+  ];
+};
